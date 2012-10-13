b47c7ddbec76158305679dec4b3ba2ede4a2879d
Fabio M. Costa <fabiomcosta@gmail.com>
2012-10-13 16:14:27 -0400

    improved check for page that is a message page, now we check for the existance of the Archive button instead of checking the URLs Hash. Fixed a bug that would lose the tabindexes after clicking on "display images below"

--- gmail-message-focus/lib/bootstrap.js
+++ gmail-message-focus/lib/bootstrap.js
@@ -8,35 +8,49 @@
         var leaderKey = ',',
             doc = $(document);
 
-        var isInboxMessage = function() {
-            return (/^#inbox[/]\w+/).test(location.hash);
+        // we are supposing that if the page contains
+        // an archive button, it is a message page
+        var isMessagePage = function() {
+            return !!doc.find('[aria-label="Archive"], [title="Archive"]').length;
         };
 
-        var isSearchMessage = function() {
-            return (/^#search[/][^/]+[/]\w+/).test(location.hash);
-        };
-
-        var checkExistanceOfElement = function(selector, callback, timeout) {
-            timeout = timeout || 200;
-            var el = $(selector);
-            if (el.length) {
-                callback(el);
+        var checkFor = function(testFunction, callback, interval) {
+            interval = interval || 200;
+            if (testFunction()) {
+                callback();
             } else {
-                setTimeout(checkExistanceOfElement.bind(this, selector, callback, timeout), timeout);
+                setTimeout(checkFor.bind(this, testFunction, callback, interval), interval);
             }
+        };
+
+        var checkExistanceOfElement = function(selector, callback, interval) {
+            checkFor(function() {
+                return doc.find(selector).length;
+            }, callback, interval);
         };
 
         var mainDetected = function() {
 
             var setupLinksTabindex = function() {
-                if (isInboxMessage() || isSearchMessage()) {
+                if (isMessagePage()) {
                     // do not get links from gmail itself
                     var links = doc.find('[role="main"] a:not([href*=".google.com/"])');
                     links.attr('tabindex', function(i) {
                         return i + 1;
                     });
+                    links.first().focus();
                 }
             };
+
+            doc.on('click', '[role="main"] :contains("Display images below")', function() {
+                // checks if all the images on the page contain a src
+                // which means that the page has been refreshed with the new
+                // html after clicking "Display images below", then setup tabindexes
+                checkFor(function() {
+                    var main = doc.find('[role="main"]');
+                    return main.find('img[src]').length === main.find('img').length;
+                }, setupLinksTabindex);
+            });
 
             $(window).on('hashchange', setupLinksTabindex);
             setupLinksTabindex();
