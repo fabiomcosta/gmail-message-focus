1f59791ea860ddb4d01953c39240ee9763495c94
Fabio M. Costa <fabiomcosta@gmail.com>
2012-11-24 12:00:19 -0500

    got too excited... I kind of rewrote the plugin, fixes #4, and is way more efficient now. also added ", ," as a shortcut to focus the first link on a message

--- gmail-message-focus/lib/bootstrap.js
+++ gmail-message-focus/lib/bootstrap.js
@@ -6,12 +6,17 @@
     if (top.document === document) {
 
         var leaderKey = ',',
-            doc = $(document);
+            doc = $(document),
+            all = document.querySelectorAll.bind(document),
+            one = document.querySelector.bind(document),
+            slice = Array.prototype.slice,
+            selectorDisplayImages = '[role="main"] :contains("Display images below")',
+            selectorLinks = '[role="main"] a:not([href*="support.google.com/"]):not([href*="mail.google.com/"])';
 
-        // we are supposing that if the page contains
+        // we suppose that if the page contains
         // an archive button, it is a message page
         var isMessagePage = function() {
-            return !!doc.find('[aria-label="Archive"], [title="Archive"]').length;
+            return !!one('[aria-label="Archive"], [title="Archive"]');
         };
 
         var checkFor = function(testFunction, callback, interval) {
@@ -23,31 +28,85 @@
             }
         };
 
-        var checkExistanceOfElement = function(selector, callback, interval) {
-            checkFor(function() {
-                return doc.find(selector).length;
-            }, callback, interval);
+        var addStyles = function(styles) {
+            $('<style>'+ styles +'</style>').appendTo('head');
         };
+
+        var Guid = function(prefix) {
+            this._cache = {};
+            this._guid = 0;
+            this.prefix = prefix || '';
+        };
+        Guid.prototype.get = function(from) {
+            if (this._cache[from]) {
+                return this._cache[from];
+            }
+            return (this._cache[from] = this.prefix + (this._guid++));
+        };
+
+        // credits http://davidwalsh.name/detect-node-insertion
+        var NodeInsertDetector = function() {
+            this.eventSource = $('<div>');
+            this._eventNames = {};
+            this.guid = new Guid('nodeInserted');
+            document.addEventListener('webkitAnimationStart', this.handler.bind(this), false);
+        };
+
+        NodeInsertDetector.prototype = {
+            constructor: NodeInsertDetector,
+
+            add: function(selector, handler) {
+                var guid = this.guid.get(selector);
+
+                if (!this._eventNames[guid]) {
+                    this._eventNames[guid] = selector;
+
+                    var styles = '\
+                        @-webkit-keyframes '+ guid +' {\
+                            from { clip: rect(1px, auto, auto, auto); }\
+                            to { clip: rect(0px, auto, auto, auto); }\
+                        }\
+                        '+ selector +' {\
+                            -webkit-animation-duration: 1ms;\
+                            -webkit-animation-name: '+ guid +';\
+                        }';
+                    addStyles(styles);
+                }
+
+                this.eventSource.on(guid, handler);
+            },
+
+            handler: function(e) {
+                var selector = this._eventNames[e.animationName];
+                if (selector) {
+                    this.eventSource.triggerHandler(e.animationName, [e, e.target, selector]);
+                }
+            }
+        };
+
 
         var mainDetected = function() {
 
             var setupLinksTabindex = function() {
                 if (isMessagePage()) {
                     // do not get links from gmail itself
-                    var links = doc.find('[role="main"] a:not([href*="support.google.com/"]):not([href*="mail.google.com/"])');
-                    links.attr('tabindex', function(i) {
-                        return i + 1;
+                    var links = slice.call(all(selectorLinks));
+
+                    links.forEach(function(el, i) {
+                        el.setAttribute('tabindex', 0);
                     });
-                    links.first().focus();
+                    if (links.length) {
+                        links[0].focus();
+                    }
                 }
             };
 
-            doc.on('click', '[role="main"] :contains("Display images below")', function() {
+            doc.on('click', selectorDisplayImages, function() {
                 // checks if all the images on the page contain a src
                 // which means that the page has been refreshed with the new
                 // html after clicking "Display images below", then setup tabindexes
                 checkFor(function() {
-                    var main = doc.find('[role="main"]');
+                    var main = $('[role="main"]');
                     return main.find('img[src]').length === main.find('img').length;
                 }, setupLinksTabindex);
             });
@@ -56,17 +115,34 @@
             setupLinksTabindex();
 
             // bind keyboardshortcuts
-            Mousetrap.init(doc.get(0));
+            Mousetrap.init(document);
 
             // displays current image
             Mousetrap.bind(leaderKey + ' i', function() {
-                doc.find('[role="main"] :contains("Display images below")').click();
+                $(selectorDisplayImages).click();
+            });
+
+            // focus first element of the email
+            // /just works on message pages
+            Mousetrap.bind(leaderKey + ' ,', function() {
+                if (isMessagePage()) {
+                    var firstLink = one(selectorLinks);
+                    if (firstLink) {
+                        firstLink.focus();
+                    }
+                }
             });
 
         };
 
-        checkExistanceOfElement('[role="main"]', mainDetected);
+        var nodeDetector = new NodeInsertDetector();
 
+        nodeDetector.add('[role="main"]', mainDetected);
+
+        // for any links added after the message has been opened
+        nodeDetector.add(selectorLinks, function(e, animEvent, element) {
+            element.setAttribute('tabindex', 0);
+        });
     }
 
 }(this));
